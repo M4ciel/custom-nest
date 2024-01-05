fd207a53540afa1e3f11d66bb2eb6410d90c48f2
Caio Maciel <caiolmaciell@gmail.com>
2024-01-05 10:51:38 -0300

    🐛 fix(microservices): class-validator, class-transformer, import terminusmodule

--- src/libs/projects/microservices/consumer/src/modules/health/health.module.ts
+++ src/libs/projects/microservices/consumer/src/modules/health/health.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
 import { HealthService } from './health.service';
+import { TerminusModule } from '@nestjs/terminus';
 
 @Module({
 	controllers: [HealthController],
+	imports: [TerminusModule],
 	providers: [HealthService],
 })
 export class HealthModule {}
--- src/libs/projects/microservices/producer/src/modules/health/health.module.ts
+++ src/libs/projects/microservices/producer/src/modules/health/health.module.ts
@@ -1,9 +1,11 @@
+import { TerminusModule } from '@nestjs/terminus';
 import { Module } from '@nestjs/common';
 import { HealthService } from './health.service';
 import { HealthController } from './health.controller';
 
 @Module({
 	controllers: [HealthController],
+	imports: [TerminusModule],
 	providers: [HealthService],
 })
 export class HealthModule {}
